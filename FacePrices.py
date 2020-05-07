f4ccbd12d1d70dd9eb2866af3ac9bd5ed257018f
Jose Pedro da Silva Gomes <57514441+oopaze@users.noreply.github.com>
2020-05-07 13:24:27 -0300

    Update FacePrices.py

--- FacePrices.py
+++ FacePrices.py
@@ -1,56 +1,78 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as bs
 from time import sleep
+try:
+    from Crawlers.MainCrawler import MainCrawler
+except ModuleNotFoundError:
+    from MainCrawler import MainCrawler
 
-
-
-class FacePrices(object):
+class FacePrices(MainCrawler):
     """
     Take the price of products on Facebook's MarketPlace searched for a key passed
     It needs you facebook login to make a more specified searching 
     """
-    def __init__(self, driver: object, arquivo: str, transition_page_time = 10):
-        self.driver = driver
-        self.produtcs = {}
+    def __init__(self, arquivo: str, driver: object):
+        super().__init__(driver)
+        
+        self.link = "https://www.facebook.com"
         self.arquivo = arquivo
-        self.transition_page_time = transition_page_time
 
     def _start(self, email, pw, key):
         """
         This function make log in the facebook login passed and go to market place
         """
+        
+        self.products = {}
+        self.test_file(self.arquivo)
+
         if email and pw:
-            self.driver.get("https://www.facebook.com/")
+            self.driver.get(self.link)
 
-            #Finding the Text box to input email and password        
-            input_email = self.driver.find_element_by_id("email")
-            input_pw = self.driver.find_element_by_id("pass")
+            try:
+                #Finding the Text box to input email and password        
+                input_email = self.driver.find_element_by_id("email")
+                input_pw = self.driver.find_element_by_id("pass")
 
-            #Putting our data logs
-            input_email.send_keys(email)
-            input_pw.send_keys(pw)      
+                #Putting our data logs
+                input_email.send_keys(email)
+                input_pw.send_keys(pw)      
+                
+                #Pressing enter and come in on facebook
+                self.driver.find_element_by_id("u_0_b").click()
+                self.login = True
+                
+            except Exception as e:
+                pass
             
-            #Pressing enter and come in on facebook
-            self.driver.find_element_by_id("u_0_b").click()
-            self.login = True
-            
-            sleep(self.transition_page_time)
+            sleep(self.transition_page+2)
             
             #Controling the loop and erros
             if self.enter_marketplace():
-                key = key.split(',')
-                for prod in key:
+                sleep(self.transition_page)
+                try:
+                    self.driver.find_element_by_css_selector('._1c2z > a:nth-child(1) > div:nth-child(1)').click()
+                    sleep(self.transition_page)
+                except Exception as e:
+                    print(e)
+                
+                for prod in key.split(','):
+
+                    sleep(self.transition_page)
+
                     if self.search_prod(prod):
                         self.take_prices()
+                        self.saving_prices(prod) #Saving data in a csv document
 
                     else:
                         print("Problemas ao procurar produtos. Tentando Novamente!")
-                        sleep(self.transition_page_time)
+                        sleep(self.transition_page)
                         self.search_prod(prod)
                         self.take_prices()
+                        self.saving_prices(prod)
             else:
                 print("Problemas ao entrar no MarketPlace. Tentando Novamente!")
-                sleep(self.transition_page_time)
+                sleep(self.transition_page)
                 self.search_prod(key)
 
         else:
@@ -64,87 +86,83 @@ class FacePrices(object):
             marketplace_button = self.driver.find_element_by_id("navItem_1606854132932955")
             marketplace_button.click()
             
-            sleep(self.transition_page_time)
+            sleep(self.transition_page)
             return True
             
         except Exception:
             return False
 
-        
     def search_prod(self, prod: str):
+        
         """
         Making our searching in Market Place with the key passed
         """
         try:
-            print(prod)
-            prod_search = self.driver.find_element_by_css_selector("#js_4 > input:nth-child(1)")
+            sleep(self.transition_page)
+            try:
+                prod_search = self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/div/div[4]/div/div/span/span/label/input")
+            except Exception as e:
+                prod_search = self.driver.find_element_by_xpath("/html/body/div[1]/div[3]/div[1]/div/div/div[2]/div[1]/div/div[4]/div/div/span/span")
+                               
             prod_search.clear()
             prod_search.send_keys(prod)
 
-            self.driver.find_element_by_class_name("_1vi5").click()
-            sleep(self.transition_page_time)
+            prod_search.send_keys(Keys.ENTER)
+
+            
             return True
             
-        except Exception:
+        except Exception as e:
             return False
 
-    
     def take_prices(self):
         """
         Save the Prices of all products found on a Dictionary like dic[product] = price
         """
-
         html = bs(self.driver.page_source, "html.parser") #taking the HTML to the page
         data = html.find_all("div", {'class':'_7yd _4-u3'}) #finding the "div" that has our info
         for e in list(data): #Running each data to take de product and the price 
-            price = e.find('div', {'class':'_f3l _4x3g'}).text.split("\xa0")
+            price = e.find('div', {'class':'_f3l _4x3g'}).text
+            
             product = e.find('img', {'class':'_7ye img'})["alt"]
-
+            product = product.replace(',', ' ')
+            image = e.find('img', {'class':'_7ye img'})['src']
+            link = None
+            
+            
             if price[0][0] == "G" or price[0][0] == "C":
-                self.produtcs[product] = "Contatar Vendedor"
+                pass
             else:
-                self.produtcs[product] = price[1]
-
-        self.saving_prices() #Saving data in a csv document
-        
-    def saving_prices(self):
+                self.products[product.replace('\n', '')] = [price, link, image]
+      
+    def saving_prices(self, key):
         """
         Save each key with its values in a csv document like:
               "dic[key] = value" to csv "key,value"
         """
         data = open(self.arquivo, 'a')
-        products = list(self.produtcs.keys())
+        products = list(self.products.keys())
         for prod in products:
-            data.write(f'{prod},{self.produtcs[prod]}\n')
+            produto = self.name_formatter(prod)
+            data.write(f'{key},{produto},{self.products[prod][0]},{self.products[prod][1]},{self.products[prod][2]},\n')
         data.close()
         
-        self.see_prices()
-
     def see_prices(self):
         """
-        Shows the products and prices in dictionary Produtcs
+        Shows the products and prices in dictionary products
         """
-        products = list(self.produtcs.keys())
+        products = list(self.products.keys())
         for prod in products:
-            print(f'{prod} - {self.produtcs[prod]}\n')
+            print(f'{prod} - {self.products[prod]}\n')
         
 
 if __name__ == '__main__':
-    arquivo = 'data.csv'
-
-    try:
-        a = open(arquivo, 'r')
-    except FileNotFoundError:
-        a = open(arquivo, 'w')
-        a.write("Product,price")
-        a.close()
-
+    arquivo = 'FBdata.csv'
+    
     key = input('Product: ')
     email = input('Facebook Email: ')
     password = input('Facebook Password: ')
+    driver = webdriver.Firefox()
 
-    firefox = webdriver.Firefox()
-
-
-    FBBot = FacePrices(firefox, arquivo)
+    FBBot = FacePrices(arquivo, driver)
     FBBot._start(email, password, key)
